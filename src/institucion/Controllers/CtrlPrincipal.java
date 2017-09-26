84e1fc858613c322a140b25b6d6c18ebcc03a756
05K4R1N <oskr.cbba2010@gmail.com>
2017-09-25 21:40:34 -0400

    [A] Interfaz Perfil para Director

---
+++ src/institucion/Controllers/CtrlPrincipal.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package institucion.Controllers;
+
+import institucion.Models.BD.PrincipalBD;
+
+/**
+ *
+ * @author o5k4r1n
+ */
+public class CtrlPrincipal {
+	private PrincipalBD mod;
+	
+	public CtrlPrincipal(){
+		mod = new PrincipalBD();
+	}
+}

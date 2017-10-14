5491f6f0acc1dfe78d5d2afa8bb1b1f4d7c11341
05K4R1N <oskr.cbba2010@gmail.com>
2017-10-13 23:51:25 -0400

    [M] Pequenio avance con disponibilidad de aula

--- src/institucion/Views/Principal/Availability.java
+++ src/institucion/Views/Principal/Availability.java
@@ -5,6 +5,9 @@
  */
 package institucion.Views.Principal;
 
+import institucion.Controllers.CtrlClassroom;
+import java.util.ArrayList;
+
 /**
  *
  * @author o5k4r1n
@@ -14,8 +17,12 @@ public class Availability extends javax.swing.JFrame {
 	/**
 	 * Creates new form Availability
 	 */
+	private CtrlClassroom ctrlC;
 	public Availability() {
+		this.setUndecorated(true);
 		initComponents();
+		this.setLocationRelativeTo(null);
+		ctrlC = new CtrlClassroom();
 	}
 
 	/**
@@ -32,6 +39,11 @@ public class Availability extends javax.swing.JFrame {
         cbmClassrooms = new javax.swing.JComboBox<>();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        addWindowListener(new java.awt.event.WindowAdapter() {
+            public void windowOpened(java.awt.event.WindowEvent evt) {
+                formWindowOpened(evt);
+            }
+        });
 
         jPanel1.setBackground(new java.awt.Color(0, 102, 0));
 
@@ -57,10 +69,21 @@ public class Availability extends javax.swing.JFrame {
         btnExit.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
         btnExit.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
         btnExit.setVerticalTextPosition(javax.swing.SwingConstants.BOTTOM);
+        btnExit.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnExitActionPerformed(evt);
+            }
+        });
 
         lblTitle.setFont(new java.awt.Font("Loma", 1, 18)); // NOI18N
         lblTitle.setForeground(new java.awt.Color(255, 255, 255));
         lblTitle.setText("ESPACIO HABILITADO EN SALA:");
+
+        cbmClassrooms.addItemListener(new java.awt.event.ItemListener() {
+            public void itemStateChanged(java.awt.event.ItemEvent evt) {
+                cbmClassroomsItemStateChanged(evt);
+            }
+        });
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
@@ -107,6 +130,26 @@ public class Availability extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void formWindowOpened(java.awt.event.WindowEvent evt) {//GEN-FIRST:event_formWindowOpened
+        ArrayList<String> classrooms = ctrlC.obtainClassrooms();
+		cbmClassrooms.addItem("Seleccione Aula");
+		for(String clasroom: classrooms){
+			cbmClassrooms.addItem(clasroom);
+		}
+    }//GEN-LAST:event_formWindowOpened
+
+    private void btnExitActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnExitActionPerformed
+		this.setVisible(false);
+    }//GEN-LAST:event_btnExitActionPerformed
+
+    private void cbmClassroomsItemStateChanged(java.awt.event.ItemEvent evt) {//GEN-FIRST:event_cbmClassroomsItemStateChanged
+        String classroom = "";
+		if(evt.getStateChange() == 1){
+			classroom = evt.getItem().toString();
+			System.out.println(classroom);
+		}
+    }//GEN-LAST:event_cbmClassroomsItemStateChanged
 
 	/**
 	 * @param args the command line arguments
